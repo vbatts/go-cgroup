ba649eaa9d84f25de6cae059d464321c6cd889c5
Yoshida <yoshida-1y-2o-3s-4h-5i-6d-7a@i.softbank.jp>
2017-04-22 19:34:12 +0900

    add AttachTaskPid

--- cg.go
+++ cg.go
@@ -221,6 +221,10 @@ func (cg Cgroup) GetUIDGID() (tasksUID UID, tasksGID GID, controlUID UID, contro
 
 }
 
+func (cg Cgroup) AttachTaskPid(pid Int) {
+	return _err(C.cgroup_attach_task_pid(cg.g, pid))
+}
+
 const (
 	// NoPerms is uninitialized file/directory permissions used for task/control files.
 	NoPerms = C.NO_PERMS
